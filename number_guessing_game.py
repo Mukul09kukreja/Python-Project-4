f369874bb72f7be505badfeebf3486848c29cc76
mukul <mukulkukreja009@gmail.com>
2026-03-06 21:24:06 +0530

    handle all exceptions and make the code run in all conditions

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,18 +1,29 @@
 import random
 
 list1 = []
-for i in range(1, 101):
+for i in range(1, 1010):
   list1.append(i)
 
 guess = random.choice(list1)
 while True:
   try:
     level = int(input("Level: "))
-    choice = int(input("Guess: "))
-    if guess == choice:
-      print("Just right")
-    if guess != choice:
-      print(f"real number is {guess}")
     break
   except ValueError:
     continue
+while True:
+  try:
+    choice0 = int(input("Guess: "))
+    if guess == choice0:
+      print("Just right !")
+      break
+    elif guess < choice0:
+      print("Too large !")
+      continue
+    elif guess > choice0:
+      print("Too Small")
+      continue
+  except ValueError:
+    continue
+  except KeyboardInterrupt:
+    print("fuck you, you can't escape it guess the number first 😎")
